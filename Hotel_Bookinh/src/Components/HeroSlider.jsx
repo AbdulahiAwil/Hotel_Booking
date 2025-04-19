522f1e2f64fd284a3a9e09182aed833ec54b64fd
AbdulahiAwil <aamosman10@gmail.com>
2025-04-19 21:52:59 +0300

    Update hero slider

--- Hotel_Bookinh/src/Components/HeroSlider.jsx
+++ Hotel_Bookinh/src/Components/HeroSlider.jsx
@@ -4,7 +4,7 @@ import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/effect-fade'
 // import required modules
-// import { EffectFade, Autoplay } from 'swiper'
+import { EffectFade, Autoplay } from 'swiper/modules'
 
 // images 
 import Img1 from '../Images/heroSlider/1.jpg'
@@ -15,41 +15,53 @@ const slides = [
     {
         title: 'Your Luxury Hotel For Vacation',
         bg: Img1,
-        btnText: 'Room & Suites',
+        btnText: 'See Our Rooms',
     },
     {
         title: 'Your Luxury Hotel For Vacation',
         bg: Img2,
-        btnText: 'Room & Suites',
+        btnText: 'See Our Rooms',
     },
     {
         title: 'Your Luxury Hotel For Vacation',
         bg: Img3,
-        btnText: 'Room & Suites',
+        btnText: 'See Our Rooms',
     },
 ]
 
 function HeroSlider() {
     
   return (
-    <Swiper className='h-[600px] lg:h-[860px]'>
+    <Swiper 
+    modules={[EffectFade, Autoplay]}
+    effect={'fade'}
+    loop={true}
+    autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+    }}
+    className='heroSlider h-[600px] lg:h-[860px]'>
         {slides.map((slide, index) => {
             const { title, bg, btnText } = slide;
             return(
-            <SwiperSlide className='h-full bg-amber-500 relative flex justify-center items-center' key={index}>
-                <div className='z-20 h-full flex justify-center items-center text-white text-center'>
+            <SwiperSlide className='h-full bg-pink-400 relative flex justify-center items-center' key={index}>
 
-                    <div>Just Enjoy and Relax</div>
-
-                </div>
-                <div className='absolute top-0 w-full h-full'>
-                    <img src={bg} className='object-cover h-full w-full' 
-                    alt="" />
-                </div>
-                {/* Overlay */}
-
-                <div className='absolute w-full h-full bg-black/70'></div>
                 
+                    
+                
+                <div className='absolute top-0 w-full h-full'>
+                    <img src={bg} alt="" className='object-cover w-full h-full'/>
+                </div>
+
+                <div className='absolute w-full h-full flex flex-col justify-center items-center bg-black/70'>
+                <div className='text-white text-center uppercase tracking-[6px] mb-5'>Just Enjoy and relax</div>
+                <h1 className='w-[800px] text-[32px] font-medium text-white text-center uppercase tracking-[2px] max-w-[920px] lg:text-[68px] leading-tight mb-6'>
+                    {title}
+                </h1>
+                <button className='text-[15px] tracking-[3px] upparcase text-white transition-all px-8 py-4 flex justify-center items-center bg-yellow-700 border-none cursor-pointer hover:bg-yellow-900'>{btnText}</button>
+                </div>
+               
+
             </SwiperSlide>
             )
         })}
