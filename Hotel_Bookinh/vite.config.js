507bb1159f5d15619853923d609984e9e977cb30
AbdulahiAwil <aamosman10@gmail.com>
2025-05-14 23:29:05 +0300

    Deploy vercel

--- Hotel_Bookinh/vite.config.js
+++ Hotel_Bookinh/vite.config.js
@@ -7,7 +7,8 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),
     tailwindcss(),
-
+    
   ],
+  base: process.env.VITE_BASE_PATH || "/Hotel_Booking",
   
 })
